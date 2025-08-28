ce7506a4bb298e55e6069539dd4f18887f2e690d
Jeremy Strange <Strange12221@gmail.com>
2025-08-28 19:54:53 -0400

    defining own function, interpolation, list

---
+++ find_extreems.py
@@ -0,0 +1,6 @@
+def find_extremes(num_list):
+    return max(num_list), min(num_list)
+
+# finding the extremes
+highest, lowest = find_extremes([1,2,3,4])
+print(f"highest= {highest} lowest = {lowest}")    
