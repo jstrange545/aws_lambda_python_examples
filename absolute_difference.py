0a7e352e3fc82f2acdf0ee23cdd2b6d2fe9dc94f
Jeremy Strange <jstrange@trevipay.com>
2025-08-28 16:41:03 -0500

    absolute difference

---
+++ absolute_difference.py
@@ -0,0 +1,6 @@
+def absolute_difference(num1, num2):
+    return abs(num1 - num2)
+
+#testing the function
+result = absolute_difference(-10, 5)
+print(f"The absolute difference between -10 and 5 is: {result}") #output should be 15
