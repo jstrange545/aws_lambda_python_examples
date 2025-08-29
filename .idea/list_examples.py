c583691404285646392b5c1993d9ecd57b43247e
Jeremy Strange <jstrange@trevipay.com>
2025-08-29 13:42:28 -0500

    a list and some code working with it

---
+++ .idea/list_examples.py
@@ -0,0 +1,9 @@
+# Creating a list
+my_list = [1, 2, 3, 'apple', 'banana', 'red', 'purple', 'orange', 10]
+print(my_list)
+
+my_list.append('grapes')
+print(my_list)
+
+my_list[0] = 'one'
+print(my_list)
