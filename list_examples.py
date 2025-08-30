081b15f193b649210c473c1cbe8126492c3c38c5
Jeremy Strange <Strange12221@gmail.com>
2025-08-30 14:27:45 -0400

    datastructures python

--- list_examples.py
+++ list_examples.py
@@ -7,3 +7,25 @@ print(my_list)
 
 my_list[0] = 'one'
 print(my_list)
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')
+
+print(f"Fruits: {fruits}, Fruits count: {fruits.count('apple')}")
+
+fruits.count('tangerine')
+
+print(f"index: {fruits.index('banana')}")
+
+fruits.index('banana', 4)  # Find next banana starting at position 4
+
+fruits.reverse()
+fruits
+
+fruits.append('grape')
+fruits
+
+fruits.sort()
+fruits
+
+fruits.pop()
